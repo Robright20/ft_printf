ca192a1c51e89a0b11db061841738d06b0e6a2da
Foka tatsinkou Robert bright <fokrober@e1r10p14.1337.ma>
2019-09-22 20:04:58 +0100

    include prototype in header

--- ft_printf.h
+++ ft_printf.h
@@ -6,11 +6,14 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 19:57:29 by fokrober          #+#    #+#             */
-/*   Updated: 2019/09/22 19:59:09 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/09/22 20:02:04 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
+
+int		ft_strnstr(char *s1, char *s2, int n);
+void	save_flag(char *flags, char *format, int argw);
 #endif
