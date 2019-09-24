e3b097afbd671348520d073f802324c6547fa70c
adamkhribeche <khribechadam@gmail.com>
2019-09-24 15:37:22 +0100

    fix error char to int

--- adam.c
+++ adam.c
@@ -6,36 +6,36 @@
 /*   By: nkhribec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:32:50 by nkhribec          #+#    #+#             */
-/*   Updated: 2019/09/24 04:47:50 by nkhribec         ###   ########.fr       */
+/*   Updated: 2019/09/24 15:36:44 by nkhribec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #define FLAG "+#- 0wpoxX"
-#define set_flag(flag, index) flag = flag | (1 << (7 - index))
-#define is_on(flag, index) (flag & (1 << (7 - index))) != 0
+#define set_flag(flag, index) flag = flag | (1 << (32 - index))
+#define is_on(flag, index) (flag & (1 << (32 - index))) != 0
 
 typedef enum 	e_flag
 {
-	PLUS, HASH, MINS, SPACE, ZERO, WIDTH, PRECISION, OCTAL, x, X
+	PLUS = 1, HASH = 2, MINS = 3, SPACE = 4, ZERO = 5, WIDTH = 6, PRECISION = 7, OCTAL = 8, x = 9, X = 10
 }				t_flag;
 
-void            printb(unsigned char c)
+void            printb(unsigned int c)
 {
 	int             i;
-	unsigned char   m;
+	unsigned int	m;
 
-	i = 8;
+	i = 32;
 	while (--i > -1)
 	{
 		m = (((c & (1 << i)) >> i) + '0');
-		write(1, &m, 8);
+		write(1, &m, 1);
 	}
 	write(1, "\n", 1);
 }
 
-/*int		ft_holder_len(char *s, char flag, int width, int precision)
+/*int		ft_holder_len(char *s, int flag, int width, int precision)
 {
 	int		len;
 	int		i;
@@ -76,7 +76,7 @@ int		ft_print(char *s, int flag, int width, int precision)
 	}
 }*/
 
-char	*ft_hash_plus_alloc_o(int *i, char flag)
+char	*ft_hash_plus_alloc_o(int *i, int flag)
 {
 	char	*s;
 	t_flag	j;
@@ -110,7 +110,7 @@ char	*ft_hash_plus_alloc_o(int *i, char flag)
 	return (s);
 }
 
-char	*ft_hash_plus_alloc_heX(int *i, char flag)
+char	*ft_hash_plus_alloc_heX(int *i, int flag)
 {
 	char	*s;
 	t_flag	j;
@@ -144,7 +144,7 @@ char	*ft_hash_plus_alloc_heX(int *i, char flag)
 	}
 	return (s);
 }
-char	*ft_hash_plus_alloc_hex(int *i, char flag)
+char	*ft_hash_plus_alloc_hex(int *i, int flag)
 {
 	char	*s;
 	t_flag	j;
@@ -179,11 +179,11 @@ char	*ft_hash_plus_alloc_hex(int *i, char flag)
 	return (s);
 }
 
-char	*ft_hash_plus_alloc(int *i, char flag)
+char	*ft_hash_plus_alloc(int *i, int flag)
 {
 	t_flag	j;
 
-	if (!(is_on(flag, (j = HASH))) && !(is_on(flag, (j = HASH))))
+	if (!(is_on(flag, (j = HASH))) && !(is_on(flag, (j = PLUS))))
 		return (ft_memalloc(sizeof(char) * ((*i) + 1)));
 	if (is_on(flag, (j = OCTAL)))
 		return (ft_hash_plus_alloc_o(i, flag));
@@ -194,11 +194,11 @@ char	*ft_hash_plus_alloc(int *i, char flag)
 	return (ft_memalloc(sizeof(char) * ((*i) + 1)));
 }
 
-//int		ft_putonbr(unsigned int n, char flag, int width, int precision)
-int		ft_putonbr(unsigned int n, char flag)
+//int		ft_putonbr(unsigned int n, int flag, int width, int precision)
+int		ft_putonbr(unsigned int n, int flag)
 {
 	char	*s;
-	char	*result;
+	char	*ret;
 	int		i;
 	int		a;
 	t_flag	j;
@@ -215,25 +215,25 @@ int		ft_putonbr(unsigned int n, char flag)
 		a /= 8;
 		i++;
 	}
-	if (!(result = ft_hash_plus_alloc(&i, flag)))
+	if (!(ret = ft_hash_plus_alloc(&i, flag)))
 		return (0);
 	while (n)
 	{
-		result[i - 1] = s[n % 8];
+		ret[i - 1] = s[n % 8];
 		n /= 8;
 		i--;
 	}
-	ft_putendl(result);
+	ft_putendl(ret);
 	ft_strdel(&s);
-	ft_strdel(&result);
+	ft_strdel(&ret);
 	return (0);
 }
 
-//int		ft_putxnbr(unsigned int n, char flag, int width, int precision)
-int		ft_putXnbr(unsigned int n, char flag)
+//int		ft_putxnbr(unsigned int n, int flag, int width, int precision)
+int		ft_putXnbr(unsigned int n, int flag)
 {
 	char	*s;
-	char	*result;
+	char	*ret;
 	int		i;
 	int		a;
 	t_flag	j;
@@ -253,24 +253,25 @@ int		ft_putXnbr(unsigned int n, char flag)
 		a /= 16;
 		i++;
 	}
-	if (!(result = ft_hash_plus_alloc(&i, flag)))
+	if (!(ret = ft_hash_plus_alloc(&i, flag)))
 		return (0);
+	printf("%s\n", ret);
 	while (n)
 	{
-		result[i - 1] = s[n % 16];
+		ret[i - 1] = s[n % 16];
 		n /= 16;
 		i--;
 	}
-	ft_putendl(result);
+	ft_putendl(ret);
 	ft_strdel(&s);
-	ft_strdel(&result);
+	ft_strdel(&ret);
 	return (0);
 }
 
-int		ft_putxnbr(unsigned int n, char flag)
+int		ft_putxnbr(unsigned int n, int flag)
 {
 	char	*s;
-	char	*result;
+	char	*ret;
 	int		i;
 	int		a;
 	t_flag	j;
@@ -290,29 +291,29 @@ int		ft_putxnbr(unsigned int n, char flag)
 		a /= 16;
 		i++;
 	}
-	if (!(result = ft_hash_plus_alloc(&i, flag)))
+	if (!(ret = ft_hash_plus_alloc(&i, flag)))
 		return (0);
 	a = i;
 	while (n)
 	{
-		result[i - 1] = s[n % 16];
+		ret[i - 1] = s[n % 16];
 		n /= 16;
 		i--;
 	}
-	ft_putendl(result);
+	ft_putendl(ret);
 	ft_strdel(&s);
-	ft_strdel(&result);
+	ft_strdel(&ret);
 	return (a);
 }
 
 int 	main()
 {
-	char flag = 0;
-	t_flag	j;
+	unsigned int	flag = 0;
+	t_flag			j;
 
-	//set_flag(flag, (j = HASH));
-	//set_flag(flag, (j = PLUS));
-	ft_putonbr(12, flag);
-	ft_putxnbr(12, flag);
-	ft_putXnbr(12, flag);
+	set_flag(flag, (j = HASH));
+	set_flag(flag, (j = PLUS));
+	set_flag(flag, (j = X));
+	printb(flag);
+	ft_putXnbr(254, flag);
 }
