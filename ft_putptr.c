34b094bf36bed00d9ba634b39ed7967536adea28
Foka tatsinkou Robert bright <fokrober@e1r10p14.1337.ma>
2019-09-25 14:45:31 +0100

    modifying putptr fonction to use the new enum

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,20 +6,18 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 21:17:13 by fokrober          #+#    #+#             */
-/*   Updated: 2019/09/25 01:57:25 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/09/25 14:41:10 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/*
 int		ft_putptr(int *flags, int width, int precision, va_list ap)
 {
 	set_flag(flags, HASH);
-	set_flag(flags, flagtoi(EL, ARGS_BUF));
+	set_flag(flags, EL);
 	return (ft_putxnbr(flags, width, precision, ap));
 }
-*/
 
 int		main(void)
 {
