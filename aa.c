2789d75b26ddbf8294b58265859d10859fb0244c
Mohamed Zaboub <mzaboub@e1r10p15.1337.ma>
2019-09-26 15:00:41 +0100

    we are satteled on the idea to work on bits (exponent mantissa sign_bit)

--- aa.c
+++ aa.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libft/libft.h"
 
-char *printfloat(float f)
+char *printfloat(float d)
 {
-	double		d = f;
+	double		f = d;
 	int			i;
-	int			com_pos;
+	int			coma_pos;
 	long long 	n;
 
-	com_pos = 0;
+	coma_pos = 0;
 	i = 0;
 	while (f > 1)
 	{
 		f *= 0.1;
-		com_pos++;
+		coma_pos++;
 	}
-	printf("%f\n", f);
-	printf("%d\n", com_pos);
-	tmp = f;
-	while (tmp - (long)f)
+	printf("f apres decalage == %f\n", f);
+	printf("come_pos == %d\n", coma_pos);
+	double	tmp = f;
+	while (tmp - (long long)tmp)
 	{
-		f *= 10;
+		tmp *= 10;
 		i++;
 	}
-	s = malloc(sizeof(char) * (i + 2));
-	while (f - (long)f != 0 && i < 19)
+	char *decim_char;
+	printf("we have found nbr of digit == %i;\n", i);
+	decim_char = malloc(sizeof(char) * (i + 2)); // +2 one for \0 and the other for dot
+	i = 0;
+	int integ;
+	while (f - (long long)f != 0 && i < 19)
 	{
 		f *= 10;
+		integ = (int)f;
+		if (i == coma_pos)
+			decim_char[i++] = '.';
+		decim_char[i] = integ + '0';
 		i++;
+		f = f - (float)integ;
+		printf("integ == %d;\n", integ);
+		
 	}
-	printf("count == %d;\n", count);
+	printf("count == %d;\n", i);
+	printf("string = %s;\n", decim_char);
 	n = (long long)f;
 
 	return (NULL);
@@ -37,6 +52,7 @@ char *printfloat(float f)
 int main()
 {
 	char *s;
-
-	s = printfloat(12.325);
+	float nbr = 123456789.325123456789;
+	s = printfloat(nbr);
+	printf("float  = %.20f;\n", nbr);
 }
